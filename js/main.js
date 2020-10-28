d5830fa576260b3bb172831c0a374e1a4f4fda21
bhothermoise1 <diaz93132@gmail.com>
2020-10-28 17:56:22 -0400

    uploading

---
+++ js/main.js
@@ -0,0 +1,37 @@
+cir = document.getElementById('cir');
+cirr=document.getElementById('cirr');
+
+function up() {
+	
+	cir.style.transform="translateY(-100px)"
+	cir.style.transition="all .5s"
+}
+
+function down() {
+	// body...
+
+	cir.style.transform="translateY(100px)"
+	cir.innerHTML=" "
+
+}
+
+function upp() {
+	cirr.style.transform="translateX(70px)"
+	cirr.style.transition="all .5s"
+}
+
+
+
+function downn() {
+	cirr.style.transform="translateX(-40px)"
+	cirr.style.transition="all .5s"
+}
+
+
+//bd.addEventListener("", function () {
+
+
+//
+//"
+//});
+
